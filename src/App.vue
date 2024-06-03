49eb1c5777245d8b38ad357062ced07633efe771
cammidelgado <camiladelgado187@gmail.com>
2024-06-03 20:33:59 -0300

    Parcial Camila Delgado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Parcial Camila Delgado</h1>
+  <h3>YA-21E NT2</h3>
+  <br>
+  <Formulario />
+
+  <hr>
+  <p>Respuestas: 1:c - 2:c  - 3:a</p>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Formulario from './components/Formulario.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Formulario
   }
 }
 </script>
 
 <style>
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 30px;
 }
 </style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,89 @@
+<template>
+
+  <section class="src-components-formulario">
+    <form @submit.prevent>
+      <input type="text" v-model="entrada">
+    </form>
+    <br>
+
+    <p>Cantidad caracteres = {{ contador }}</p>
+
+    <p>1 - {{ codificado }} (codificado)</p>
+    <p>2 - {{ mayuscula }} (mayuscula)</p>
+    <p>3 - {{ minuscula }} (minuscula)</p>
+    <p>4 - {{ mayusMin }} (mayuscula / minuscula)</p>
+    <p>5 - {{ minMayus }} (minuscula / mayuscula)</p>
+
+
+
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-formulario',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        entrada: "",
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+      codificado() {
+        return this.entrada.toLowerCase().replace(/[aeiou]/g, (vocal) =>{
+          switch (vocal){
+            case 'a':
+              return 'u'
+            case 'e':
+              return 'o'
+            case 'o':
+              return 'e'
+            case 'u': 
+              return 'a'
+          }
+        })
+      },
+
+      mayuscula() {
+        return this.entrada.toUpperCase()
+      },
+
+      minuscula() {
+        return this.entrada.toLowerCase()
+      },
+
+      mayusMin() {
+        return [...this.entrada].map((char, index) =>
+        index % 2 ? char.toLowerCase() : char.toUpperCase()
+      ).join('')
+
+      },
+
+      minMayus() {
+        return [...this.entrada].map((char, index) =>
+        index % 2 ? char.toUpperCase() : char.toLowerCase()
+      ).join('')
+      },
+
+      contador(){
+        return this.entrada.length
+      }
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  
+</style>
